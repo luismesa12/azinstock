2f9f40bd4e5d97b8ae77e83668bb4bb96ccc4881
luismesa12 <luismesa12@hotmail.com>
2021-02-14 18:26:33 -0500

    Se añade función eliminar Item de Inventario

--- js/purchases.js
+++ js/purchases.js
@@ -78,7 +78,6 @@ function llenarInputItemPorId(event) {
 
     document.querySelector(idNombreProducto).value = productoPorId(valueInputId).nombre;
     document.querySelector(idstock).value = productoPorId(valueInputId).stock;
-
 }
 function productoPorId(nid) {
     let producto;
@@ -189,3 +188,46 @@ function addToStock() {
 const btnAddToStock = document.querySelector("#addToStock");
 btnAddToStock.addEventListener("click", addToStock);
 /*Fin Añadir Nuevo Item a Inventario */
+
+/*Eliminar Item */
+const idDelProducto = document.querySelector("#idDelProducto");
+const delProducto = document.querySelector("#delProducto");
+const btndelStock = document.querySelector('#delStock');
+idDelProducto.addEventListener("keyup", productById);
+btndelStock.addEventListener("click", deleteItem)
+let index;
+function productById(e) {
+    idDelProducto.oninput = ()=> {
+        if (this.value.length > 4) {
+            this.value = this.value.slice(0, 4);
+        };
+    };
+    let idFound = false;
+    index = undefined;
+    dbStock = JSON.parse(localStorage.getItem("DBstock"));
+    dbStock.forEach(i => {
+        if (i.id === Number(e.target.value)) {
+            console.log(e.target.value,"borrar");
+            delProducto.value = i.nombre;
+            idFound = true;
+            index = dbStock.indexOf(i);
+        }
+    });
+    if (!idFound) {
+        delProducto.value = "Id No Existe En Stock";
+    };
+};
+function deleteItem() {
+        if (index) {
+            dbStock = JSON.parse(localStorage.getItem("DBstock"));
+            dbStock.splice(index,1);
+            localStorage.setItem("DBstock", JSON.stringify(dbStock));
+            index = undefined;
+            alert("Se eliminó El Item")
+        }
+        else{
+            alert("Id No Existe En Stock")
+        }
+}
+/*Fin Eliminar Item */
+
