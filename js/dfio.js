b5823b71547da36aae15c541c505dd8fe12cf366
luismesa12 <luismesa12@hotmail.com>
2021-02-16 20:50:24 -0500

    iconos

--- js/dfio.js
+++ js/dfio.js
@@ -156,11 +156,11 @@ function facturar() {
         ¡¡¡Gracias Por Su Compra!!!<br>
         ---------------------------------------`;
         const part9 = document.createElement("span");
-        part9.innerHTML = `<button type="button" id="verCredito" class="btn btn-primary mx-2" data-bs-toggle="modal" data-bs-target="#modalCredito">
+        part9.innerHTML = `<button type="button" id="verCredito" class="btn btn-outline-dark mx-2" data-bs-toggle="modal" data-bs-target="#modalCredito">
         Ver Crédito</button>`
 
         const part10 = document.createElement("span");
-        part10.innerHTML = `<button type="button" id="print" class="btn btn-primary">
+        part10.innerHTML = `<button type="button" id="print" class="btn btn-outline-dark">
         Imprimir Factura</button>`
 
         const divPrueba = document.querySelector('#primerDiv');
